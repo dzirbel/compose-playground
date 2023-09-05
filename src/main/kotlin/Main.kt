75a698d4490c2e41c3a65cb40e8fc14cb2fdbf3e
Dominic Zirbel <dominiczirbel@gmail.com>
2023-09-04 20:47:43 -0700

    Create maximized window demo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,71 +1,52 @@
-import androidx.compose.foundation.focusable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.runtime.*
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusEvent
-import androidx.compose.ui.input.key.key
-import androidx.compose.ui.input.key.onKeyEvent
-import androidx.compose.ui.input.key.type
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 
-@Composable
-fun App() {
-    var parentKeyEvent by remember { mutableStateOf("") }
-    var parentFocusEvent by remember { mutableStateOf("") }
-
-    MaterialTheme {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .focusable()
-                .onFocusEvent {
-                    parentFocusEvent = it.toString()
-                }
-                .onKeyEvent {
-                    parentKeyEvent = "${it.key} ${it.type}"
-                    false
-                }
-        ) {
-            Column(
-                modifier = Modifier.padding(8.dp),
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-            ) {
-                var textFieldValue1 by remember { mutableStateOf("") }
-                TextField(
-                    value = textFieldValue1,
-                    onValueChange = { textFieldValue1 = it },
-                    singleLine = true,
-                )
-
-                var textFieldValue2 by remember { mutableStateOf("") }
-                TextField(
-                    value = textFieldValue2,
-                    onValueChange = { textFieldValue2 = it },
-                    singleLine = true,
-                )
-
-                Button(onClick = { /* no-op */ }) {
-                    Text("Dummy button (for focus)")
-                }
-
-                Text("Last parent key event: $parentKeyEvent")
-
-                Text("Last parent focus event: $parentFocusEvent")
-            }
-        }
-    }
-}
-
+const val MAXIMIZED_INITIAL = false
 fun main() {
     application {
-        Window(onCloseRequest = ::exitApplication) {
-            App()
+        var maximized by remember { mutableStateOf(MAXIMIZED_INITIAL) }
+
+        val windowState = if (maximized) {
+            WindowState(
+                placement = WindowPlacement.Maximized,
+                size = DpSize.Unspecified,
+                position = WindowPosition.Aligned(Alignment.Center),
+            )
+        } else {
+            WindowState(
+                placement = WindowPlacement.Floating,
+                size = DpSize(300.dp, 800.dp),
+                position = WindowPosition.Aligned(Alignment.Center),
+            )
+        }
+
+        Window(onCloseRequest = ::exitApplication, state = windowState) {
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .layout { measurable, constraints ->
+                        println("layout $constraints")
+                        val placeable = measurable.measure(constraints)
+                        layout(placeable.width, placeable.height) { placeable.place(0, 0) }
+                    }
+                    .clickable { maximized = !maximized }
+            )
         }
     }
 }
